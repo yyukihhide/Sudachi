0a0f31bacbc81574b1133fdd1db9b877157811f7
yyuki <yyuki@gmail.com>
2016-02-29 09:26:36 +0900

    初

---
+++ app/src/main/java/com/bazan/mobile10/sudachi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bazan.mobile10.sudachi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
